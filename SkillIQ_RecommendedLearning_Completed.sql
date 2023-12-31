cdbdc5fb60fc6a167eb43cdeb6ec5e8a1a8c004a
nehapaul-np <116820533+nehapaul-np@users.noreply.github.com>
2023-12-31 13:11:34 +0530

    Added query for clip watchduration & clip length

--- SkillIQ_RecommendedLearning_Completed.sql
+++ SkillIQ_RecommendedLearning_Completed.sql
@@ -1,5 +1,4 @@
 -- Recommended module
-
 WITH cte1 AS (
     SELECT a.*,
         b.TITLE AS Module_title,
@@ -14,8 +13,7 @@ WITH cte1 AS (
         AND a.CONTENTID LIKE '5a91f08b-5cf0-41f2-99a4-569fac3ce091'
 )
 SELECT DISTINCT *
-FROM cte1;
-
+FROM cte1
 
 --Module duration
 WITH cte1 AS (
@@ -40,25 +38,24 @@ SELECT MODULEID,
 FROM cte2
 GROUP BY 1,
     2,
-    3
-
-
+    3 
+    
 --Module & Clip level Watch duration
-WITH cte1 AS (
-    SELECT date(LASTWATCHUTC) AS watchdate,
-        USERHANDLE,
-        MODULEID,
-        CLIPID,
-        TOTALVIEWTIMEINSECONDS
-    FROM SOURCE_SYSTEM.PRODUCT.COURSE_PROGRESS_DETAIL
-    WHERE COURSEID = '7d70de87-4066-466b-8be2-e5db345b6d07'
-        AND ARCHIVEDCLIP = 'Active Clip'
-        AND MODULEID LIKE '5a91f08b-5cf0-41f2-99a4-569fac3ce091'
-),
-cte2 AS(
-    SELECT DISTINCT *
-    FROM cte1
-)
+    WITH cte1 AS (
+        SELECT date(LASTWATCHUTC) AS watchdate,
+            USERHANDLE,
+            MODULEID,
+            CLIPID,
+            TOTALVIEWTIMEINSECONDS
+        FROM SOURCE_SYSTEM.PRODUCT.COURSE_PROGRESS_DETAIL
+        WHERE COURSEID = '7d70de87-4066-466b-8be2-e5db345b6d07'
+            AND ARCHIVEDCLIP = 'Active Clip'
+            AND MODULEID LIKE '5a91f08b-5cf0-41f2-99a4-569fac3ce091'
+    ),
+    cte2 AS(
+        SELECT DISTINCT *
+        FROM cte1
+    )
 SELECT watchdate,
     MODULEID,
     USERHANDLE,
@@ -69,25 +66,25 @@ WHERE USERHANDLE LIKE '2e5c07ae-a4b1-4df4-a297-158a3e6d81a4'
 GROUP BY 1,
     2,
     3,
-    4
-
+    4 
+    
 --Module & Clip level Watch duration along with clip length
-WITH cte1 AS (
-    SELECT date(LASTWATCHUTC) AS watchdate,
-        USERHANDLE,
-        MODULEID,
-        CLIPID,
-        TOTALVIEWTIMEINSECONDS,
-        CLIPLENGTHINSECONDS
-    FROM SOURCE_SYSTEM.PRODUCT.COURSE_PROGRESS_DETAIL
-    WHERE COURSEID = '7d70de87-4066-466b-8be2-e5db345b6d07'
-        AND ARCHIVEDCLIP = 'Active Clip'
-        AND MODULEID LIKE '5a91f08b-5cf0-41f2-99a4-569fac3ce091'
-),
-cte2 AS(
-    SELECT DISTINCT *
-    FROM cte1
-)
+    WITH cte1 AS (
+        SELECT date(LASTWATCHUTC) AS watchdate,
+            USERHANDLE,
+            MODULEID,
+            CLIPID,
+            TOTALVIEWTIMEINSECONDS,
+            CLIPLENGTHINSECONDS
+        FROM SOURCE_SYSTEM.PRODUCT.COURSE_PROGRESS_DETAIL
+        WHERE COURSEID = '7d70de87-4066-466b-8be2-e5db345b6d07'
+            AND ARCHIVEDCLIP = 'Active Clip'
+            AND MODULEID LIKE '5a91f08b-5cf0-41f2-99a4-569fac3ce091'
+    ),
+    cte2 AS(
+        SELECT DISTINCT *
+        FROM cte1
+    )
 SELECT watchdate,
     MODULEID,
     USERHANDLE,
@@ -100,3 +97,5 @@ GROUP BY 1,
     2,
     3,
     4
+
+--next step to add the clip watch to the recommended
