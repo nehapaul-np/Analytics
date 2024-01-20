fcce8a27ea1cddbf58819538ce9ccead9931855f
nehapaul-np <116820533+nehapaul-np@users.noreply.github.com>
2024-01-20 14:29:04 +0530

    Final Version Includes only the B2B Customers from the Plan User table

--- %B2B_users_with_atleast_one_company_channel.sql
+++ %B2B_users_with_atleast_one_company_channel.sql
@@ -1,3 +1,4 @@
+--All users including B2B & B2C
 WITH plan_user_count AS (
 SELECT
 	YEAR(to_timestamp(CREATEDAT,
@@ -32,3 +33,63 @@ JOIN org_channel_user_count b ON
 	a.YEAR = b.YEAR
 WHERE
 	a.YEAR > 2021;
+
+
+--Final Version Includes only the B2B Customers from the Plan User table
+WITH plan_user_count AS (
+WITH B2B_Plans AS (
+SELECT
+	DISTINCT plan_id
+FROM
+	ANALYTICS.CERTIFIED.B2B_SUBSCRIPTIONS
+),
+
+cte2 AS (
+SELECT
+	DATE(TO_TIMESTAMP(a.CREATEDAT,
+	3)) AS memberadded_date,
+	COALESCE(a.USERHANDLE,
+	a.PENDINGEMAIL) AS USERHANDLE,
+	a.PLANID
+FROM
+	DVS.CURRENT_STATE.skills_plans_v1_PlanUser a
+WHERE
+	a.PLANID IN (
+	SELECT
+		plan_id
+	FROM
+		B2B_Plans))
+		
+SELECT
+	YEAR(memberadded_date) AS yeardate,
+	COUNT(DISTINCT USERHANDLE) AS b2bplanuser
+FROM
+	cte2
+GROUP BY
+	1
+),
+
+org_channel_user_count AS (
+SELECT
+	YEAR(Member_added_date) AS YEAR,
+	COUNT(DISTINCT userhandle) AS ChannelUser
+FROM
+	ANALYTICS.SANDBOX.NE_CHANNEL_B2B_USERS_ASSIGNED_WITH_ORG_CHANNEL
+WHERE
+	PRIVACYLEVEL = 'org'
+	AND BUSINESS_TYPE LIKE 'B2B'
+GROUP BY
+	1
+)
+
+SELECT
+	a.yeardate,
+	a.b2bplanuser,
+	b.ChannelUser,
+	ROUND((CAST(b.ChannelUser AS DECIMAL) / NULLIF(a.b2bplanuser, 0)) * 100) AS B2BUserPercentage
+FROM
+	plan_user_count a
+JOIN org_channel_user_count b ON
+	a.yeardate = b.YEAR
+WHERE
+	a.yeardate > 2021;
